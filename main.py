f331f6dcf3dc9ec2b798cead1844fa4900fa2fcd
rachel ombok <rbo224@nyu.edu>
2020-09-11 19:59:29 -0400

    create flask app

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+    
+@app.route("/home")
+def salvador():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
