493f6b005ce5eb0457a8ae659be5589f042dd3e0
Lawrence <lawrence.d.williams.0@gmail.com>
2020-09-11 21:55:57 -0400

    Added Rough Class Page Outline

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
-    
+
 @app.route("/home")
 def salvador():
     return render_template('home.html')
+
+@app.route("/class")
+def course():
+    return render_template('class.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
