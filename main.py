6e71d3a52d67037654f1b7932b6506599cab08c9
Lawrence <lawrence.d.williams.0@gmail.com>
2020-09-13 01:34:39 -0400

    Added Custom Class Page Form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -9,6 +9,29 @@ def home():
 @app.route("/home")
 def salvador():
     return render_template('home.html')
+
+@app.route("/create_class")
+def create_class():
+    return render_template('create_class.html')
+
+@app.route("/custom_course", methods=["GET","POST"])
+def custom_class():
+
+    if request.method == "POST":
+
+        req = request.form
+
+        #Collecting the form values
+        #course_code = req.get("course_code")
+        #course_title = req.get("course_title")
+        #start_time = req.get("start_time")
+        #end_time = req.get("end_time")
+        #professor_name = req.get("professor_name")
+        #email = req.get("email")
+        #website_link = req.get("website_link")
+        #syllabus_link = req.get("syllabus_link")
+
+    return render_template('custom_course.html', req = req)
 
 @app.route("/class")
 def course():
